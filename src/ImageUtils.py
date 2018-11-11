9b8b89f84ef0f7b70c8272f8e7eb258ad29c8985
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-11 11:59:08 -0200

    convert to grayscale and normalize image

--- src/ImageUtils.py
+++ src/ImageUtils.py
@@ -1,13 +1,16 @@
 import os
 from openface.openface import data
+import cv2
+import numpy as np
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
 
 def to_rgb_array(image_buffer):
-    tmp_image = open('image.bin', 'wb')
-    tmp_image.write(image_buffer)
-    print(os.path.join(fileDir, '..', 'image.bin'))
-    rgb = data.Image('tmp', 'imagetmp', os.path.join(fileDir, '..', 'image.bin')).getRGB()
-    tmp_image.close()
-    os.remove('image.bin')
-    return rgb
+    return cv2.imdecode(np.array(image_buffer), cv2.IMREAD_COLOR)
+
+def to_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+def normalize(image):
+    normalized_image = np.zeros((800, 800))
+    return cv2.normalize(image,  normalized_image, 0, 255, cv2.NORM_MINMAX)
