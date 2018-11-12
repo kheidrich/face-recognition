93ca3b8a1843bc86ccf419a75f4dead86f230122
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-12 01:59:40 -0200

    model paths in config.json

--- src/RecogitionService.py
+++ src/RecogitionService.py
@@ -1,11 +1,15 @@
 import os
-from openface.openface import AlignDlib, TorchNeuralNet, data
+import json
+from openface import AlignDlib, TorchNeuralNet, data
 
-fileDir = os.path.dirname(os.path.realpath(__file__))
-modelDir = os.path.join(fileDir, '..', 'models')
-face_landmark_model_file = os.path.join(modelDir, 'shape_predictor_68_face_landmarks.dat')
+with open('./src/config.json') as file:
+    config = json.load(file)
 
-face_aligner = AlignDlib(face_landmark_model_file)
+face_aligner = AlignDlib(config['faceLandmarksModelPath'])
+feature_extractor = TorchNeuralNet(config['featureExtractionModelPath'])
 
-def align_face(rgb_image):
-    return face_aligner.align(96, rgb_image)
+def align_face(image):
+    return face_aligner.align(96, image)
+
+def extract_features(image):
+    return feature_extractor.forward(image)
