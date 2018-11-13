6d3f0f6bcf2b278260f502f8f3f4a7f377d0d204
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-13 00:19:34 -0200

    WIP: train model

--- src/server.py
+++ src/server.py
@@ -1,12 +1,25 @@
 import os
 from flask import Flask, request, jsonify
 import ImageUtils
+import RecogitionService
+import cv2
 
 app = Flask("recognition-service")
 
 @app.route('/train', methods=['POST'])
 def handle_train_model():
-    print(ImageUtils.to_rgb_array(bytearray(request.json['face']['data'])))
+    image = ImageUtils.to_rgb_array(bytearray(request.json['face']['data']))
+    grayscale_image = ImageUtils.rgb_to_grayscale(image)
+    normalized_image = ImageUtils.normalize(grayscale_image)
+    aligned_face = ImageUtils.grayscale_to_rgb(RecogitionService.align_face(normalized_image))
+
+    tmp_image = open('/mnt/c/teste/tmp.jpg', 'wb')
+    (done, encoded) = cv2.imencode('.jpg', aligned_face)
+    tmp_image.write(encoded)
+    tmp_image.close()
+
+    print(RecogitionService.extract_features(aligned_face))
+
     return jsonify({"model": {"data": [22,22,22,22], "type": "Buffer"}})
 
 @app.route('/recognize', methods=['POST'])
