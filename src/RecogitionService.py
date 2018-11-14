0f0e771f5f40efc53b0f96a023999e8594cb223a
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-14 17:36:49 -0200

    WIP: recognice face from features

--- src/RecogitionService.py
+++ src/RecogitionService.py
@@ -1,6 +1,9 @@
 import os
 import json
+import cv2
 from openface import AlignDlib, TorchNeuralNet, data
+import NegativeSamples
+import numpy as np
 
 with open('./src/config.json') as file:
     config = json.load(file)
@@ -11,5 +14,23 @@ feature_extractor = TorchNeuralNet(config['featureExtractionModelPath'])
 def align_face(image):
     return face_aligner.align(96, image)
 
+
 def extract_features(image):
     return feature_extractor.forward(image)
+
+def recognize(face_features, face_features_to_recognize):
+    NEGATIVE = 0
+    POSITIVE = 1
+    samples = NegativeSamples.load()
+    train_data = []
+    labels = []
+    knn = cv2.ml.KNearest_create()
+    for sample in samples:
+        features = np.array(extract_features(align_face(sample)))
+        train_data.append(np.array(features, dtype='f'))
+        labels.append(NEGATIVE)
+    train_data.append(np.array(face_features, dtype='f'))
+    labels.append(POSITIVE)
+    knn.train(np.array(train_data), cv2.ml.ROW_SAMPLE, np.array(labels))
+    print knn.findNearest(np.array([np.array(face_features_to_recognize, dtype='f')]), 3)
+    return False
--- src/server.py
+++ src/server.py
@@ -2,12 +2,12 @@ import os
 from flask import Flask, request, jsonify
 import ImageUtils
 import RecogitionService
-import cv2
+import numpy as np
 
 app = Flask("recognition-service")
 
 @app.route('/features', methods=['POST'])
-def handle_train_model():
+def handle_extract_face_features():
     image = ImageUtils.decode_image_buffer(bytearray(request.json['face']['data']))
     grayscale_image = ImageUtils.rgb_to_grayscale(image)
     normalized_image = ImageUtils.normalize(grayscale_image)
@@ -17,11 +17,15 @@ def handle_train_model():
     return jsonify({"features": list(face_features)})
 
 @app.route('/recognize', methods=['POST'])
+def handle_recognize_face_from_features():
+    features = np.array(request.json['features'])
+    face_to_recognize = ImageUtils.decode_image_buffer(bytearray(request.json['face']['data']))
+    grayscale_image = ImageUtils.rgb_to_grayscale(face_to_recognize)
+    normalized_image = ImageUtils.normalize(grayscale_image)
+    aligned_face = ImageUtils.grayscale_to_rgb(RecogitionService.align_face(normalized_image))
+    face_to_recognize_features = RecogitionService.extract_features(aligned_face)
+    are_same = RecogitionService.recognize(features, face_to_recognize_features)
 
-    return jsonify({"model": {"data": [22,22,22,22], "type": "Buffer"}})
-
-@app.route('/recognize', methods=['POST'])
-def handle_compare_image_with_model():
-    return jsonify({"areSame": True})
+    return jsonify({"areSame": are_same})
 
 app.run(port=4003)
