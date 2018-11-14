e0d62322f4241397634b64b805c36c2b1e99fe11
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-14 14:49:28 -0200

    get face features

--- src/server.py
+++ src/server.py
@@ -6,19 +6,17 @@ import cv2
 
 app = Flask("recognition-service")
 
-@app.route('/train', methods=['POST'])
+@app.route('/features', methods=['POST'])
 def handle_train_model():
-    image = ImageUtils.to_rgb_array(bytearray(request.json['face']['data']))
+    image = ImageUtils.decode_image_buffer(bytearray(request.json['face']['data']))
     grayscale_image = ImageUtils.rgb_to_grayscale(image)
     normalized_image = ImageUtils.normalize(grayscale_image)
     aligned_face = ImageUtils.grayscale_to_rgb(RecogitionService.align_face(normalized_image))
+    face_features = RecogitionService.extract_features(aligned_face)
 
-    tmp_image = open('/mnt/c/teste/tmp.jpg', 'wb')
-    (done, encoded) = cv2.imencode('.jpg', aligned_face)
-    tmp_image.write(encoded)
-    tmp_image.close()
+    return jsonify({"features": list(face_features)})
 
-    print(RecogitionService.extract_features(aligned_face))
+@app.route('/recognize', methods=['POST'])
 
     return jsonify({"model": {"data": [22,22,22,22], "type": "Buffer"}})
 
