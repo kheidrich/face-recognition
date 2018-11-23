5752b55754b46184fed825e8809cc1fb83bc054e
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-23 17:37:29 -0200

    add more negative samples

--- src/NegativeSamples.py
+++ src/NegativeSamples.py
@@ -1,7 +1,11 @@
 import cv2
 
+images = []
+for img in range(0,39):
+    images.append(cv2.imread('src/negative-face-samples/1~' + str(img)  + '.jpg'))
+
+for img in range(1,15):
+    images.append(cv2.imread('src/negative-face-samples/1~' + str(img)  + '.jpg'))
+
 def load():
-    images = []
-    for img in range(1,15):
-        images.append(cv2.imread('src/negative-face-samples/subject' + str(img)  + '.jpg'))
     return images
