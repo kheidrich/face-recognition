6b0c9af94b5aae03af1e3e79d355e5890a083d9f
Kevin Pinzon <kevin.pinzon@hotmail.com>
2018-11-27 08:25:25 -0200

    change negative face examples

--- src/NegativeSamples.py
+++ src/NegativeSamples.py
@@ -1,11 +1,7 @@
 import cv2
 
-images = []
-for img in range(0,39):
-    images.append(cv2.imread('src/negative-face-samples/1~' + str(img)  + '.jpg'))
-
-for img in range(1,15):
-    images.append(cv2.imread('src/negative-face-samples/1~' + str(img)  + '.jpg'))
-
 def load():
+    images = []
+    for img in range(1,57):
+        images.append(cv2.imread('src/negative-face-samples/' + str(img)  + '.jpg'))
     return images
